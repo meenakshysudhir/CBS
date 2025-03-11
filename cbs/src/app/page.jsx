41956875ef9a21875da2c5c37903b90b1a5d3b69
Meenakshy Sudhir <ukms2004@gmail.com>
2025-03-11 16:22:03 +0530

    font changed

--- cbs/src/app/page.jsx
+++ cbs/src/app/page.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import {Label} from "@/components/ui/label"
 import {Input} from "@/components/ui/input.jsx"   
 import {Button} from "@/components/ui/button"
+
+import { Courier_Prime } from 'next/font/google'
+
+const courierPrime = Courier_Prime({
+  weight: ['400', '700'], // Specify font weights
+  subsets: ['latin'], 
+  display: 'swap',
+})
+
 const page = () => {
   return (
-    <div className='flex justify-center items-center h-screen bg-gray-100'>
+    <div className='flex justify-center items-center h-screen bg-gray-100 {courierPrime.className}'>
       <form className='flex flex-col gap-4 p-15 w-1/4 border-black bg-white-500 rounded-xl'>
       <Label>Username</Label>
       <Input placeholder="trkrishna" type="text" id="username" name="username"/>
