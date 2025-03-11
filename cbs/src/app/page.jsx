99c861877aa280aee268a2b1fd5e89383e8dbfb7
Meenakshy Sudhir <ukms2004@gmail.com>
2025-03-11 15:03:39 +0530

    l

--- cbs/src/app/page.jsx
+++ cbs/src/app/page.jsx
@@ -5,14 +5,15 @@ import {Button} from "@/components/ui/button"
 const page = () => {
   return (
     <div className='flex justify-center items-center h-screen bg-gray-100'>
-      <div className="flex flex-col gap-4 p-15 w-1/4 border-black bg-white-500 rounded-xl">
-      
+      <form className='flex flex-col gap-4 p-15 w-1/4 border-black bg-white-500 rounded-xl'>
       <Label>Username</Label>
       <Input placeholder="trkrishna" type="text" id="username" name="username"/>
       <Label>Password</Label>
       <Input placeholder="****" type="password" id="password" name="password"/>
-      <Button>Login</Button>
-      </div>
+      <Button
+      //  onClick={()=>{LogIn()}}
+       >Login</Button>
+      </form>
     </div>
   )
 }
